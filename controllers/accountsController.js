11c0030229882b242e79fd2a0e946f82c968da57
Eduardo Oliveira <santos.edu3301@gmail.com>
2021-10-18 14:47:52 -0300

    account-info page added and responsive

--- controllers/accountsController.js
+++ controllers/accountsController.js
@@ -38,3 +38,17 @@ exports.getEditAccountPage = (req, res, next) => {
     })
 
 }
+
+exports.getAccountInfoPage = (req, res, next) => {
+
+    const service = req.query.service
+    const id = req.query.id
+
+    res.render('account-info', {
+
+        service: service,
+        id: id
+
+    })
+
+}
