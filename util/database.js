0aa427fd8eb10bd62221614eed16d150af9242a3
Eduardo Oliveira <82374791+devEdu-web@users.noreply.github.com>
2021-10-23 19:48:38 -0300

    Update database.js

--- util/database.js
+++ util/database.js
@@ -1,7 +1,1 @@
-const {Sequelize} = require('sequelize')
-const sequelize = new Sequelize('passwords', 'root', '5uCLpccG@J30282230$', {
-    host: 'localhost',
-    dialect: 'mysql'
-})
 
-module.exports = sequelize
